a6c0cc20fb2ea585c5accd7eaec80e79df2964ef
Martin Voelkle <martin dot voelkle at gmail dot com@localhost>
2008-08-03 21:30:55 +0200

    fixed script error position

---
+++ libusl/src/interpreter.cpp
@@ -0,0 +1,117 @@
+#include "interpreter.h"
+
+#include "types.h"
+#include "usl.h"
+#include "code.h"
+
+#include <iostream>
+
+using namespace std;
+
+/*
+Thread::RuntimeValues::RuntimeValues():
+	trueValue(0),
+	falseValue(0)
+{}
+
+Value* Thread::getRuntimeValue(Value*& cachedValue, const std::string& name)
+{
+	if (cachedValue == 0)
+		cachedValue = getRootLocal(name);
+	return cachedValue;
+}
+
+Value* Thread::getRootLocal(const std::string& name)
+{
+	ScopePrototype* rootPrototype = root->scopePrototype();
+	ScopePrototype::Locals::const_iterator it = find(rootPrototype->locals.begin(), rootPrototype->locals.end(), name);
+	assert(it != rootPrototype->locals.end());
+	size_t index = it - rootPrototype->locals.begin();
+	return root->locals[index];
+}
+*/
+
+bool Thread::step()
+{
+	if (state == RUN)
+	{
+		Thread::Frame& frame = frames.back();
+		ThunkPrototype* thunk = frame.thunk->thunkPrototype();
+		size_t nextInstr = frame.nextInstr;
+		Code* code = thunk->body[nextInstr];
+		frame.nextInstr++;
+		
+		cout << thunk;
+		for (size_t i = 0; i < frames.size(); ++i)
+			cout << "[" << frames[i].stack.size() << "]";
+		cout << " " << usl->debug.find(thunk, nextInstr) << ": ";
+		code->dump(cout);
+		cout << endl;
+		
+		code->execute(this);
+		
+		while (true)
+		{
+			Thread::Frame& frame = frames.back();
+			if (frame.nextInstr < frame.thunk->thunkPrototype()->body.size())
+				break;
+			Value* retVal = frame.stack.back();
+			frames.pop_back();
+			if (!frames.empty())
+			{
+				frames.back().stack.push_back(retVal);
+			}
+			else
+			{
+				state = STOP;
+				break;
+			}
+		}
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+size_t Thread::run(size_t maxSteps)
+{
+	size_t steps;
+	for (steps = 0; steps < maxSteps; ++steps)
+	{
+		if (!step())
+			break;
+	}
+	return steps;
+}
+
+size_t Thread::run()
+{
+	size_t steps;
+	for (steps = 0; true; ++steps)
+	{
+		if (!step())
+			break;
+	}
+	return steps;
+}
+
+void Thread::markForGC()
+{
+	using namespace std;
+	using namespace __gnu_cxx;
+	
+	// mark all frames in stack
+	for_each(frames.begin(), frames.end(), mem_fun_ref(&Frame::markForGC));
+}
+
+void Thread::Frame::markForGC()
+{
+	using namespace std;
+	using namespace __gnu_cxx;
+	
+	// mark all variables in frame
+	for_each(stack.begin(), stack.end(), mem_fun(&Value::markForGC));
+	thunk->markForGC();
+}
